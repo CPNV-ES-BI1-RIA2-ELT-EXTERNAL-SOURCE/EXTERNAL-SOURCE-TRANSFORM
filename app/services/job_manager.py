dbb6a017456c6764ccb635fe338fb6a326494383
Sebastien-Moraz <sebastien@moraz.net>
2025-01-23 16:31:26 +0100

    feat: add remove oldest job in JobManager for flush storage

--- app/services/job_manager.py
+++ app/services/job_manager.py
@@ -7,6 +7,8 @@ class JobManager:
     def store_job(job_id: int, job_data: dict):
         if not os.path.exists("jobs"):
             os.makedirs("jobs")
+        if JobManager.count_jobs() >= 1000:
+            JobManager.remove_oldest_job()
         file = open(f"jobs/{job_id}.json", "w")
         file.write(json.dumps(job_data, indent=4))
         file.close()
@@ -36,3 +38,15 @@ class JobManager:
             os.makedirs("jobs")
         for file in os.listdir("jobs"):
             os.remove(f"jobs/{file}")
+
+    @staticmethod
+    def count_jobs() -> int:
+        return len(os.listdir("jobs"))
+
+    @staticmethod
+    def remove_oldest_job():
+        if JobManager.count_jobs() == 0:
+            return
+        jobs = os.listdir("jobs")
+        oldest_job = min(jobs, key=lambda job: os.path.getctime(f"jobs/{job}"))
+        os.remove(f"jobs/{oldest_job}")
